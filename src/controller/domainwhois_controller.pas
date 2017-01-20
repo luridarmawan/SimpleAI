4ef767a1b60c82d92d8901444d4852c865631bf6
ulil <luri@kioss.com>
2017-01-20 11:14:19 +0700

    move domainwhois library to simpleai controller #1

---
+++ src/controller/domainwhois_controller.pas
@@ -0,0 +1,119 @@
+unit domainwhois_controller;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  whois_integration, dateutils, common, RegExpr, fpjson,
+  Classes, SysUtils;
+
+const
+  _WHOIS_CACHE_PATH = 'ztemp/cache/domains/';
+  _WHOIS_CACHE_EXTENSION = '.txt';
+
+type
+
+  { TDomainWhoisController }
+
+  TDomainWhoisController = class
+  private
+    FCache: boolean;
+    Whois: TWhoisIntegration;
+  public
+    constructor Create;
+    destructor Destroy;
+
+    property Cache: boolean read FCache write FCache;
+    function Find(DomainName, Option: string): string;
+  end;
+
+implementation
+
+{ TDomainWhoisController }
+
+constructor TDomainWhoisController.Create;
+begin
+  FCache := True;
+end;
+
+destructor TDomainWhoisController.Destroy;
+begin
+end;
+
+function TDomainWhoisController.Find(DomainName, Option: string): string;
+var
+  cacheFile: string;
+  forceWhois: boolean;
+  i: integer;
+begin
+  if not isDomain(DomainName) then
+  begin
+    Result := 'sepertinya nama domain tidak valid';
+    Exit;
+  end;
+
+  Result := 'finding ' + domainName;
+
+  cacheFile := _WHOIS_CACHE_PATH + domainName + _WHOIS_CACHE_EXTENSION;
+
+  Whois := TWhoisIntegration.Create;
+  try
+    forceWhois := False;
+    if FileExists(cacheFile) then
+    begin
+      i := HoursBetween(FileDateToDateTime(FileAge(cacheFile)), now);
+      if i = 0 then
+        Whois.Data.LoadFromFile(cacheFile)
+      else
+        forceWhois := True;
+    end
+    else
+      forceWhois := True;
+
+    if forceWhois then
+    begin
+      if not Whois.Find(DomainName) then
+      begin
+        Result := 'Informasi whois domain ' + DomainName + ' tidak ditemukan';
+        Exit;
+      end;
+      if not FCache then
+        Whois.Data.SaveToFile(cacheFile);
+    end;
+
+    Result := 'Domain: ' + UpperCase(domainName)
+      + '\nRegistrar: ' + Whois.Registrar
+      + '\nRegistrant: ' + Whois.Registrant
+      + '\nStatus: ' + Whois.Status
+      + '\nUpdated Date: ' + Whois.UpdatedDate
+      + '\nCreation Date: ' + Whois.CreationDate
+      + '\nExpiration Date: ' + Whois.ExpiredDate
+      + '\nName Server: ' + Whois.NameServer;
+    if ((Option = 'full') or (Option = 'lengkap')) then
+    begin
+      Result := StringToJSONString(Whois.Data.Text);
+    end;
+  except
+    on E: Exception do
+    begin
+      Result := 'failed: ' + E.Message;
+    end;
+  end;
+  Whois.Free;
+end;
+
+end.
+
+
+
+
+
+
+
+
+
+
+
+
+
