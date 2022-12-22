e52ccb3db6e1007f16f203bc77bddd611dcc757c
Luri Darmawan <luri@kioss.com>
2022-12-23 06:01:07 +0700

    fix duplicate response text

--- source/package/src/simpleai_controller.pas
+++ source/package/src/simpleai_controller.pas
@@ -1252,8 +1252,8 @@ begin
     itemAsArray.Add(trim(FResponseText[i]));
   end;
   s := trim(CustomReply['text']);
-  if not s.IsEmpty then
-    itemAsArray.Add(s);
+  //if not s.IsEmpty then
+  //  itemAsArray.Add(s);
   o.ValueArray['response/text'] := itemAsArray;
   o['response/elapsed_time'] := ElapsedTime.ToString;
   if FMsg <> '' then
